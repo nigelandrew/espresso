525c6e35e63532ce79fca33d2f62d0cd3ebe2401
Nigel <nigelrandrew@gmail.com>
2025-05-12 08:41:05 -0400

    add CoffeeTable.tsx and RoasterTable.tsx

---
+++ src/components/RoasterTable.tsx
@@ -0,0 +1,14 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table.tsx"
+import { downloadCSV } from "@/utils/exportCSV";
+import {Button} from "@/components/ui/button.tsx";
+
+export default RoasterTable({}) {
+
+};
